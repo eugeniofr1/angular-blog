0e60f0d41bc2ec77f6e51b60bb00a382cb38c6a0
Eugênio Francisco da Silva <eugeniofr17@gmail.com>
2023-09-21 14:36:50 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -13,6 +13,9 @@ export class SmallCardComponent implements OnInit {
   cardTitle:string = ''
   @Input()
   cardDescription:string = ''
+
+  @Input()
+  Id:string = '0'
   constructor() { }
 
   ngOnInit(): void {
